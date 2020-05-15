5bb8a5d7a540d43f8ddcb167e0627d01949c8f4d
Matthew Conlin <32144021+conlin-matt@users.noreply.github.com>
2020-05-15 15:43:09 -0400

    Fix silly mistake in main.py

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -2473,7 +2473,7 @@ class performRectificationThread(QThread):
 # Launch the tool #       
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    w = rectify_InputsWindow()
+    w = WelcomeWindow()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
 
